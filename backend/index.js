2e1dc66b4d1359b0a10a171789ba2cab131455f8
HypeDeveloper <de.hypedeveloper@gmail.com>
2022-12-31 09:26:24 +0100

    update hosting

--- backend/index.js
+++ backend/index.js
@@ -24,9 +24,9 @@ app.use(errHandler);
 
 // pack Frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+    app.use(express.static(path.join(__dirname, './dist')))
     app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html'))
+        res.sendFile(path.resolve(__dirname, './dist', 'index.html'))
     })
 } 
 else {
